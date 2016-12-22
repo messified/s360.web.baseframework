3b024fe12ec65566520ae82bca3c4ef7b71ab985
messified <jesse.c.reese@gmail.com>
2016-12-22 10:00:02 -0800

    updated routes and included demo component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,17 @@
-import { Routes, RouterModule } from '@angular/router';
-import { DataResolver } from './app.resolver';
+import {Routes, RouterModule} from '@angular/router';
+import {DataResolver} from './app.resolver';
 
 
 export const ROUTES: Routes = [
-  {
-    path: '', loadChildren: () => System.import('./components/customer')
-      .then((comp: any) => comp.default),
-  }
+	{
+		path: '', redirectTo: 'app', pathMatch: 'full'
+	},
+	{
+		path: 'app', loadChildren: () => System.import('./components/demo')
+		.then((comp: any) => comp.default),
+	},
+	{
+		path: 'app/customer', loadChildren: () => System.import('./components/customer')
+			.then((comp: any) => comp.default),
+	}
 ];
---
+++ src/app/components/demo/base.component.ts
@@ -0,0 +1,11 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+	selector: 'base',
+	templateUrl: 'base.template.html',
+	encapsulation: ViewEncapsulation.None,
+})
+
+export class Base {
+	constructor (){}
+}
